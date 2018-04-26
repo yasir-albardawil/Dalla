a616cce2865b86157f48ab432eb55a7ebc5fdddd
yasir-albardawil <yasser1600@gmail.com>
2018-04-26 10:14:42 +0300

    Initial commit

--- src/net/buammar/تشغيل.java
+++ src/net/buammar/تشغيل.java
@@ -5,7 +5,7 @@ package net.buammar;
 public class تشغيل {
     public static void main(String[] args) {
         الدلة قهوة = new الدلة( "قهوة" , 500, 50);
-        قهوة.تقديم(1, 0);
+        قهوة.تقديم(11, 1);
         قهوة.كمية_السائل_في_الدلة();
     }
 }
